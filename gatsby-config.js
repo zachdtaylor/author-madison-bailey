4de95ae0190d74646b2d25ea9250602d8c5ef602
Zach Taylor <zachtylr04@gmail.com>
2020-07-18 23:37:49 -0500

    Change site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Author Madison Bailey`,
+    description: `Madison Bailey is an author writing clean romances for the hopeful romantic.`,
+    author: `@authormadisonbailey`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
