2d299478e77ea4ee51071cdfeed27ee2fe02de38
Zach Taylor <zachtylr04@gmail.com>
2020-07-19 11:39:24 -0500

    Delete starter css

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,11 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
