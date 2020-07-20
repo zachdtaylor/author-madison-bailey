9715c17d636b4a65398ae1d2836ef8b1936b4283
Zach Taylor <zachtylr04@gmail.com>
2020-07-19 21:27:15 -0500

    Begin index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,33 @@
-import React from "react"
+import React from 'react'
+import { graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Img from 'gatsby-image'
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = ({ data }) => (
+  <div>
     <SEO title="Home" />
-  </Layout>
+    <Img fluid={data.headerImage.childImageSharp.fluid} />
+    <div className="px-6 py-3">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Clean Romances for the
+        <span className="text-red-300"> Hopeful Romantic</span>
+      </h1>
+    </div>
+  </div>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query HeaderImageQuery {
+    headerImage: file(relativePath: { eq: "Madison-Bailey-Logo-1.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1240) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
