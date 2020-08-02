55245a5250235c06d7c265547c54434671d487e6
Zach Taylor <zachtylr04@gmail.com>
2020-08-01 19:55:19 -0500

    Create paragraph component

---
+++ src/components/paragraph.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Paragraph = ({ children }) => {
+  return (
+    <p className="text-gray-900 text-lg leading-relaxed py-2">{children}</p>
+  )
+}
+
+export default Paragraph
