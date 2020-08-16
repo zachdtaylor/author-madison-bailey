0b827994755e9c6baa4218a8b7f301f9354f805b
Zach Taylor <zachtylr04@gmail.com>
2020-08-16 16:04:21 -0500

    Finish first draft of mobile nav bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,6 +2,8 @@ import PropTypes from 'prop-types'
 import React, { useState } from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import NavBarItem from './nav-bar-item'
+import MenuIcon from './menu-icon'
 
 const Header = () => {
   const [mobileMenuActive, setMobileMenuActive] = useState(false)
@@ -17,34 +19,40 @@ const Header = () => {
     }
   `)
   return (
-    <header id="top" className="mx-auto p-5">
-      <nav id="site-menu" className="flex-row md:justify-between">
-        <div className="flex flex-row justify-between">
+    <header id="top" className="mx-auto">
+      <nav id="site-menu" className="flex-row md:flex md:justify-between">
+        <div className="flex flex-row justify-between px-2 py-1 pr-4 shadow-md">
           <Link to="/">
             <Img
               fluid={data.headerImage.childImageSharp.fluid}
               className="w-16"
             />
           </Link>
-          <p
+          <div
             id="hamburgerbtn"
-            className="md:hidden"
+            className="w-10 relative md:hidden"
             onClick={() => setMobileMenuActive(!mobileMenuActive)}
           >
-            menu
-          </p>
+            <div className="absolute bottom-0 top-0">
+              <MenuIcon />
+            </div>
+          </div>
         </div>
         <ul
-          className={`hidden md:flex md:flex-row ${
+          className={`hidden my-3 mx-5 md:flex md:flex-row ${
             mobileMenuActive && 'active'
           }`}
         >
-          <li className="pr-5">
-            <Link to="/books">Hi</Link>
-          </li>
-          <li className="pr-5">
-            <Link to="/books">Books</Link>
-          </li>
+          <Link to="/" className="w-full">
+            <NavBarItem>
+              <span className="text-xl">Home</span>
+            </NavBarItem>
+          </Link>
+          <Link to="/books" className="w-full">
+            <NavBarItem>
+              <span className="text-xl">Books</span>
+            </NavBarItem>
+          </Link>
         </ul>
       </nav>
     </header>
---
+++ src/components/nav-bar-item.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NavBarItem = ({ children }) => {
+  return <li className="py-4 border-b-2">{children}</li>
+}
+
+export default NavBarItem
