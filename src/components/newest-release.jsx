9f5078bbc7cdda65955beee9384c236a8de5c89f
Zach Taylor <zachtylr04@gmail.com>
2020-11-06 23:08:19 -0600

    Hook newest release up to contentful

--- src/components/newest-release.jsx
+++ src/components/newest-release.jsx
@@ -5,17 +5,33 @@ import Paragraph from '../components/paragraph'
 import Button from './button'
 
 const NewestRelease = () => {
-  const data = useStaticQuery(graphql`
+  const { books } = useStaticQuery(graphql`
     query {
-      hemiolaImage: file(relativePath: { eq: "hemiola-cover.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 800) {
-            ...GatsbyImageSharpFluid_tracedSVG
+      books: allContentfulBook(
+        sort: { fields: [publicationDate], order: DESC }
+        limit: 1
+      ) {
+        edges {
+          node {
+            title
+            description {
+              content {
+                content {
+                  value
+                }
+              }
+            }
+            coverArt {
+              fluid {
+                ...GatsbyContentfulFluid_tracedSVG
+              }
+            }
           }
         }
       }
     }
   `)
+  const newestBook = books.edges[0].node
   return (
     <div className="px-6 py-2">
       <div className="pt-8 pb-5">
@@ -24,22 +40,19 @@ const NewestRelease = () => {
       <div className="md:mx-8 lg:mx-32">
         <h2 className="text-3xl pb-3 pr-12">Read My Newest Release</h2>
         <div className="border-gray-700 rounded-md shadow-md overflow-hidden md:grid md:grid-cols-2 md:gap-4 md:rounded-none md:shadow-none">
-          <Img fluid={data.hemiolaImage.childImageSharp.fluid} />
+          <Img fluid={newestBook.coverArt.fluid} />
           <div className="p-4 pb-0">
-            <h1 className="text-2xl font-bold">Hemiola</h1>
-            <Paragraph>
-              Micah Moore is young, handsome, and newly famous. Riding high from
-              his recent success as the lead in a box office hit, the world is
-              his oyster. To him every door is open, and there are no
-              consequences he can’t charm his way out of.
-            </Paragraph>
-            <Paragraph>
-              But after a drunken car crash lands him in the hospital, Micah and
-              his twin sister, Melanie, are sent to live with their aunt in
-              Kimball Junction, Utah, for the ultimate attitude adjustment.
-            </Paragraph>
+            <h1 className="text-2xl font-bold">{newestBook.title}</h1>
+            {newestBook.description.content
+              .slice(0, 2)
+              .map(({ content }, index) => (
+                <Paragraph key={index}>{content[0].value}</Paragraph>
+              ))}{' '}
             <div className="text-right md:text-left">
-              <Button.Link to="/books/hemiola" className="mt-3 mb-5 md:my-3">
+              <Button.Link
+                to={`/books/${newestBook.title.toLowerCase()}`}
+                className="mt-3 mb-5 md:my-3"
+              >
                 Read More
               </Button.Link>
             </div>
