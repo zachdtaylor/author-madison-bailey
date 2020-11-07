42f91e41a84ed4c3a751b3e67c23321675dfc2d7
Zach Taylor <zachtylr04@gmail.com>
2020-11-07 17:44:47 -0600

    Put max height on purchase icons

--- src/components/purchase-link.jsx
+++ src/components/purchase-link.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PurchaseLink = ({ link, imgURL, altText }) => {
   return (
     <a href={link} className="self-center">
-      <img src={imgURL} alt={altText} className="w-40" />
+      <img src={imgURL} alt={altText} className="max-h-12" />
     </a>
   )
 }
