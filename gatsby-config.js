bf3e0af8809d7d597fe4483632a3f51099751985
Zach Taylor <zachtylr04@gmail.com>
2020-11-06 23:54:03 -0600

    Update manifest plugin options

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `author-madison-bailey`,
+        short_name: `author`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffb3b3`,
+        theme_color: `#ffb3b3`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Madison-Bailey-Submark.png`, // This path is relative to the root of the site.
       },
     },
     {
