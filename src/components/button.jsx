15327f4d7fed31549d9613b2a465b04b34957671
Zach Taylor <zachtylr04@gmail.com>
2020-11-11 22:22:05 -0600

    Add hover animation to button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -5,7 +5,7 @@ const Button = {
   Link: ({ to, children, className }) => (
     <Link
       to={to}
-      className={`inline-block bg-pink hover:bg-pink-light text-white py-3 px-4 rounded shadow-md ${className}`}
+      className={`inline-block bg-pink hover:bg-pink-light ease-in-out transition duration-500 text-white py-3 px-4 rounded shadow-md ${className}`}
     >
       {children}
     </Link>
